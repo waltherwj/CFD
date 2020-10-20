31063b5b335ff3a7b94c5f2fd3f4e6cb31ec3561
walther.johnson <walther.johnson@ucalgary.ca>
2020-10-19 18:04:02 -0600

    initial commit

---
+++ q3_solution_walther.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+This script can be run on any Python intepreter to generate the answers to Q3 in HW2 of ENME 572
+Walther Wennholz Johnson - walther.johnson@ucalgary.ca
+"""
+
